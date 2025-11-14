f75afbc6e6d6734ec64a636b2355555b8a54f7d0
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-11-15 00:43:34 +0530

    New Changes

--- server/src/controllers/jsonWebToken-Config.ts
+++ server/src/controllers/jsonWebToken-Config.ts
@@ -1,11 +1,6 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
 
 if (!process.env.SECRET_KEY) {
     throw new Error("FATAL ERROR: SECRET_KEY is not defined.");
